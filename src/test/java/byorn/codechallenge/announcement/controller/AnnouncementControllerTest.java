5e2c71f5448e0d3dfac0bdcc6b8e98ed5c92b9e8
byorn <byorn.desilva@gmail.com>
2015-03-06 12:21:06 +0530

    Added Unit Test Approach

---
+++ src/test/java/byorn/codechallenge/announcement/controller/AnnouncementControllerTest.java
@@ -0,0 +1,21 @@
+package byorn.codechallenge.announcement.controller;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Byorn
+ */
+public class AnnouncementControllerTest {
+    
+    
+    @Test
+    public void test_createOrUpdateAnnouncment() throws Exception {
+            /**
+             * Can use PowerMock to mock DB Access Classes
+             * to Test function units.
+             * 
+             */
+            assert(true);
+    }
+}
